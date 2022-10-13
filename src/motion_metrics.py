e465fad54fefbb518c96eb3a352367d317144e67
jyang-2 <jyang2@caltech.edu>
2022-10-12 20:55:29 -0700

    Added functions for saving motion correction results in a more interpretable format

--- src/motion_metrics.py
+++ src/motion_metrics.py
@@ -1,16 +1,20 @@
+import sys
+
+sys.path.append("/home/remy/PycharmProjects/CaimanAssist/src")
 import numpy as np
 from pathlib import Path
 from typing import Tuple, List, Literal, Union, Optional
 import pydantic
-import typing
 from itertools import product
 import json
 import pandas as pd
-import pprint as pp
-import utils2p
+import argparse
 
 
-# %%
+# import typing
+# import pprint as pp
+# import utils2p
+
 
 class PatchShifts3dRigid(pydantic.BaseModel):
     dims: List[int]
@@ -133,15 +137,15 @@ def get_3d_rig_shifts(mc_rig):
     shifts_xmin, shifts_ymin, shifts_zmin = shifts.max(axis=0).tolist()
 
     return PatchShifts3dRigid(
-        dims=dims,
-        overlaps=overlaps,
-        strides=strides,
-        shifts_xmin=shifts_xmin,
-        shifts_xmax=shifts_xmax,
-        shifts_ymin=shifts_ymin,
-        shifts_ymax=shifts_ymax,
-        shifts_zmin=shifts_zmin,
-        shifts_zmax=shifts_zmax, )
+            dims=dims,
+            overlaps=overlaps,
+            strides=strides,
+            shifts_xmin=shifts_xmin,
+            shifts_xmax=shifts_xmax,
+            shifts_ymin=shifts_ymin,
+            shifts_ymax=shifts_ymax,
+            shifts_zmin=shifts_zmin,
+            shifts_zmax=shifts_zmax, )
 
 
 def get_3d_els_patch_shifts(mc_els):
@@ -180,25 +184,25 @@ def get_3d_els_patch_shifts(mc_els):
     patch_x, patch_y, patch_z = zip(*patch_idx_arr)
 
     return PatchShifts3dElastic(
-        dims=dims,
-        overlaps=overlaps,
-        strides=strides,
-        patch_x=patch_x,
-        patch_y=patch_y,
-        patch_z=patch_z,
-        shifts_xmin=shifts_xmin.tolist(),
-        shifts_xmax=shifts_xmax.tolist(),
-        shifts_ymin=shifts_ymin.tolist(),
-        shifts_ymax=shifts_ymax.tolist(),
-        shifts_zmin=shifts_zmin.tolist(),
-        shifts_zmax=shifts_zmax.tolist(), )
+            dims=dims,
+            overlaps=overlaps,
+            strides=strides,
+            patch_x=patch_x,
+            patch_y=patch_y,
+            patch_z=patch_z,
+            shifts_xmin=shifts_xmin.tolist(),
+            shifts_xmax=shifts_xmax.tolist(),
+            shifts_ymin=shifts_ymin.tolist(),
+            shifts_ymax=shifts_ymax.tolist(),
+            shifts_zmin=shifts_zmin.tolist(),
+            shifts_zmax=shifts_zmax.tolist(), )
 
 
 def get_rig_shift_lims(patch_shifts):
     return ShiftLimits3d(
-        x=[patch_shifts.shifts_xmin, patch_shifts.shifts_xmax],
-        y=[patch_shifts.shifts_ymin, patch_shifts.shifts_ymax],
-        z=[patch_shifts.shifts_zmin, patch_shifts.shifts_zmax],
+            x=[patch_shifts.shifts_xmin, patch_shifts.shifts_xmax],
+            y=[patch_shifts.shifts_ymin, patch_shifts.shifts_ymax],
+            z=[patch_shifts.shifts_zmin, patch_shifts.shifts_zmax],
     )
 
 
@@ -225,14 +229,14 @@ def get_els_shift_lims(patch_shifts):
     """
     df = patch_shifts.to_dataframe()
     return ShiftLimits3d(
-        x=[df.loc[df['patch_x'] == df['patch_x'].min(), 'shifts_xmax'].max(),
-           df.loc[df['patch_x'] == df['patch_x'].max(), 'shifts_xmin'].min()],
+            x=[df.loc[df['patch_x'] == df['patch_x'].min(), 'shifts_xmax'].max(),
+               df.loc[df['patch_x'] == df['patch_x'].max(), 'shifts_xmin'].min()],
 
-        y=[df.loc[df['patch_y'] == df['patch_y'].min(), 'shifts_ymax'].max(),
-           df.loc[df['patch_y'] == df['patch_y'].max(), 'shifts_ymin'].min()],
+            y=[df.loc[df['patch_y'] == df['patch_y'].min(), 'shifts_ymax'].max(),
+               df.loc[df['patch_y'] == df['patch_y'].max(), 'shifts_ymin'].min()],
 
-        z=[df.loc[df['patch_z'] == df['patch_z'].min(), 'shifts_zmax'].max(),
-           df.loc[df['patch_z'] == df['patch_z'].max(), 'shifts_zmin'].min()]
+            z=[df.loc[df['patch_z'] == df['patch_z'].min(), 'shifts_zmax'].max(),
+               df.loc[df['patch_z'] == df['patch_z'].max(), 'shifts_zmin'].min()]
     )
 
 
@@ -260,17 +264,35 @@ def main(moco_dir):
                                els_shift_lims=els_shift_limits)
     moco_metrics.save_json(moco_dir.joinpath('moco_metrics.json'))
     print('moco_metrics.json saved.')
-    return True
+    return moco_metrics
 
 
 # %%
 if __name__ == '__main__':
-    NAS_PROC_DIR = Path("/local/storage/Remy/narrow_odors/processed_data")
-    folder_list = sorted(list(NAS_PROC_DIR.rglob('caiman_mc')))
-    for folder in folder_list:
-        print(folder)
-        try:
-            main(folder)
-        except Exception as e:
-            print("motion correction metrics did not run sucessfully.")
-            print(e)
+    # NAS_PROC_DIR = Path("/local/matrix/Remy-Data/projects/natural_mixtures/processed_data")
+    #
+    # folder_list = sorted(list(NAS_PROC_DIR.rglob('kiwi_components_again_with_partial_and_probes/caiman_mc')))
+    # for folder in folder_list:
+    #     print(folder)
+    #     try:
+    #         main(folder)
+    #     except Exception as e:
+    #         print("motion correction metrics did not run sucessfully.")
+    #         print(e)
+
+    USAGE = f"writes shifts from caiman's 3d motion correction to .../<moco_dir>/<els, " \
+            f"rig>_patch_shifts.json,\n" \
+            "and writes motion metrics to .../<moco_dir>/moco_metrics.json"
+    parser = argparse.ArgumentParser(description=USAGE)
+    parser.add_argument('moco_dir', type=str,
+                        help='path to caiman motion correction results folder, probably w/ name '
+                             'caiman_mc')
+    args = parser.parse_args()
+
+    folder = Path(args.moco_dir)
+
+    try:
+        main(folder)
+    except Exception as e:
+        print("motion_metrics.py did not run sucessfully.")
+        print(e)
